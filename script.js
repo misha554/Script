5b7a0ce781f47b6e241b355410b889d70b1db04f
misha554 <131030933+misha554@users.noreply.github.com>
2023-04-17 21:28:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let day = "вівторок"
+
+alert("Це перша сторінка сайта")
+
+alert("yt 3"/2)
+alert(`Сьогодні ${day}`)
+let result = prompt("Яке сьогодні число?","14.03.2023" )
+alert(`Було введено: ${result}`)
+let result = confirm ("Сьогодні точно вівторок?")
+alert (result)
+
+let a = prompt ("Введіть число")
+alert (`Добуток числа ${a} і 10 дорівнює ${a*10}`)
